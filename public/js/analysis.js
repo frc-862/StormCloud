76c296e4aa34cede570f54bc2377a2f6381c6775
David Reeves <djreeve693@pccsk12.com>
2023-02-05 22:36:09 -0500

    Initial chart library and viewing bug fix

---
+++ public/js/analysis.js
@@ -0,0 +1,46 @@
+var overlaySaveData = {};
+var overlaySaveFunction = ()=>{};
+
+function get(link, headers, callback){
+    $.ajax({
+        url: link,
+        type: "GET",
+        headers: headers,
+        success: function(data){
+            callback(true, data);
+        },
+        error: function(err){
+            callback(false, err);
+        }
+    })
+}
+
+function post(link, headers, data, callback){
+    $.ajax({
+        url: link,
+        type: "POST",
+        headers: headers,
+        data: data,
+        success: function(data){
+            callback(true, data);
+        },
+        error: function(err){
+            callback(false, err);
+        }
+    })
+}
+
+function deleteAPI(link, headers, data, callback){
+    $.ajax({
+        url: link,
+        type: "DELETE",
+        headers: headers,
+        data: data,
+        success: function(data){
+            callback(true, data);
+        },
+        error: function(err){
+            callback(false, err);
+        }
+    })
+}
--- public/js/general.js
+++ public/js/general.js
@@ -1059,7 +1059,9 @@ function handle_document_click(id){
                                         `;
                                         break;
                                 }
-                                n += 1;
+                                if(c.Type != "Label"){
+                                    n += 1;
+                                }
                                 
                             });
         
@@ -1937,6 +1939,32 @@ Array.from(document.getElementsByClassName("changeScreen")).forEach((item)=>{
 })
 
 
-
+function show_analysis_chart_demo(){
+    const data = {
+        labels: ["12am-3am", "3am-6pm", "6am-9am", "9am-12am",
+            "12pm-3pm", "3pm-6pm", "6pm-9pm", "9am-12am"
+        ],
+        datasets: [
+            {
+                name: "Some Data", type: "bar",
+                values: [25, 40, 30, 35, 8, 52, 17, -4]
+            },
+            {
+                name: "Another Set", type: "line",
+                values: [25, 50, -10, 15, 18, 32, 27, 14]
+            }
+        ]
+    }
+    
+    const chart = new frappe.Chart("#analysis_chart", {  // or a DOM element,
+                                                // new Chart() in case of ES6 module with above usage
+        title: "My Awesome Chart",
+        data: data,
+        type: 'axis-mixed', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
+        height: 400,
+        colors: ['#7cd6fd', '#743ee2']
+    })
+}
+show_analysis_chart_demo();
 
 
