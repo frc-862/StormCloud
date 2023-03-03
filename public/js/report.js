0f27e408982779e8987bd8f54a4920b543673cb8
David Reeves <djreeve693@pccsk12.com>
2023-03-03 14:36:34 -0500

    Add fan page and ensure stability

--- public/js/report.js
+++ public/js/report.js
@@ -33,6 +33,21 @@ var team = urlParams.get('teamNumber');
 if(team == null || team == undefined){
     team = prompt("Please enter a team number to get the match analysis for");
 }
+var fan  = urlParams.get('fan');
+if(fan == null || fan == undefined){
+    fan = "no";
+}
+
+var textColor = fan == "yes" ? "white" : "#190024";
+if(fan == "yes"){
+    document.body.style.backgroundColor = "transparent";
+    document.querySelector("html").style.backgroundColor = "#323996";
+    document.querySelector("html").style.backgroundImage = "linear-gradient(90deg, #20256b 0%, #323996 100%)";
+    document.querySelector("html").style.backgroundRepeat = "no-repeat";
+    document.querySelector("html").style.backgroundSize = "cover";
+    
+
+}
 
 
 
@@ -45,7 +60,7 @@ function getMatchAnalysis(){
         var fHTML = "";
         fHTML += `
         <div class="flex_center">
-            <span class="header" style="color:#190024;margin-bottom:20px">Matches for ${team}</span>
+            <span class="header" style="color:${textColor};margin-bottom:20px">Matches for ${team}</span>
         </div>
         `;
 
@@ -62,15 +77,17 @@ function getMatchAnalysis(){
             }
 
             var mHTML = `
-            <div class="flex_apart" style="margin:10px"> 
+            <div class="flex_apart${fan == "yes" ? " container" : ""}" style="${fan == "yes" ? "margin:10px;padding:20px 40px;border-radius:16px;background-color:rgba(0,0,0,0.5);" : ""}"> 
             `;
 
             mHTML += `
-            <span class="important" style="color:#190024">Match ${match.matchNumber}</span>
+            <span class="important" style="color:${textColor}">Match ${match.matchNumber}</span>
             `
 
             var redTeams = match.teams.filter(t => t.color == "Red");
             var blueTeams = match.teams.filter(t => t.color == "Blue");
+
+            var ourTeam = match.teams.filter(t => t.team == team)[0];
 
             var redHTML = ``;
             var blueHTML = ``;
@@ -79,14 +96,14 @@ function getMatchAnalysis(){
             redTeams.forEach(function(t){
 
                 redHTML += `
-                <div class="redbg container flex_center" style="margin:5px; padding:5px; border-radius:12px;width:100px">
+                <div class="redbg container flex_center" style="margin:5px; padding:5px; border-radius:12px;width:100px;${t.team == team ? "border: 2px solid white" : "border:2px solid transparent"}">
                     <span class="important" style="color:white">${t.team == team ? `<u>${t.team}</u>` : `${t.team}`}</span>
                 </div>
                 `;
             });
             blueTeams.forEach(function(t){
                 blueHTML += `
-                <div class="bluebg container flex_center" style="margin:5px; padding:5px; border-radius:12px;width:100px">
+                <div class="bluebg container flex_center" style="margin:5px; padding:5px; border-radius:12px;width:100px;${t.team == team ? "border: 2px solid white" : "border:2px solid transparent"}">
                     <span class="important" style="color:white">${t.team == team ? `<u>${t.team}</u>` : `${t.team}`}</span>
                 </div>
                 `;
@@ -102,19 +119,19 @@ function getMatchAnalysis(){
             </div>
             
             `
-
-
             if(match.results.finished){
                 var redScore = match.results.red;
                 var blueScore = match.results.blue;
 
+                var winner = redScore > blueScore ? "Red" : (redScore == blueScore ? "Tie" : "Blue");
+
                 mHTML += `
-                <div class="${redScore > blueScore ? "redbg" : "bluebg"} container" style="margin:10px; padding:10px; border-radius:12px;width:120px">
+                <div class="${redScore > blueScore ? "redbg" : (redScore == blueScore ? "primarybg":"bluebg")} container" style="margin:10px; padding:10px; border-radius:12px;width:160px;${winner == ourTeam.color ? "border: 2px solid white" : "border: 2px solid transparent"}">
                     <div class="flex_center">
                         <span class="important" style="color:white">${redScore} - ${blueScore}</span>
                     </div>
                     <div class="flex_center">
-                        <span class="regular" style="color:white">${redScore > blueScore ? "Red" : "Blue"} Wins</span>
+                        <span class="regular" style="color:white">${redScore > blueScore ? "Red Wins" : (redScore == blueScore ? "It's a Tie!":"Blue Wins")}</span>
                     </div>
                     
                     
@@ -122,11 +139,14 @@ function getMatchAnalysis(){
                 `
             }else{
                 mHTML += `
-                <div class="whitebg container flex_center" style="margin:10px; padding:10px; border-radius:12px;width:120px">
-                    <span class="important" style="color:black">${match.matchNumber - prevMatch.matchNumber} Matches</span>
+                <div class="whitebg container flex_center" style="margin:10px; padding:10px; border-radius:12px;width:160px">
+                    <span class="important" style="color:white">Not Played</span>
                 </div>
                 `
             }
+
+
+            
 
             mHTML += `</div>`;
 
@@ -138,3 +158,7 @@ function getMatchAnalysis(){
     });
 }
 getMatchAnalysis();
+
+setInterval(function(){
+    getMatchAnalysis();
+}, 30000);
