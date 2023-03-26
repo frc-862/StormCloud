e6840aec6237552db7bb9fa0190c66a0b9f1c023
David Reeves <djreeve693@pccsk12.com>
2023-03-25 21:50:40 -0400

    Match scores not multiplied by 10

--- public/js/genanalysis.js
+++ public/js/genanalysis.js
@@ -1154,7 +1154,7 @@ function selectAnalysis(){
                                                 break;
                                         }
 
-                                        matchFinal.data = Math.round(matchFinal.data*10)/1;
+                                        matchFinal.data = Math.round(matchFinal.data*10)/10;
 
                                         final.push(matchFinal);
                                     });
